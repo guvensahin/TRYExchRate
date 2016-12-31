d9772d40d725b4138d5ddb95bd85877af6cf3720
guvensahin <guvensahin@outlook.com>
2016-12-31 13:27:17 +0300

    initial

---
+++ src/TRYExchRate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TRYExchRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exchRateHelper = new TRYExchRate(DateTime.Now);
+
+            decimal test = exchRateHelper.GetExchRate("USD", ExchRateType.ForexBuying);
+
+
+            Console.Write(exchRateHelper.CurrencyDate);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            // USD
+            Console.WriteLine("USD");
+            Console.WriteLine("Forex Buying: " + exchRateHelper.GetExchRate("USD", ExchRateType.ForexBuying).ToString());
+            Console.WriteLine("Forex Selling: " + exchRateHelper.GetExchRate("USD", ExchRateType.ForexSelling).ToString());
+            Console.WriteLine("Banknote Buying: " + exchRateHelper.GetExchRate("USD", ExchRateType.BanknoteBuying).ToString());
+            Console.WriteLine("Banknote Selling: " + exchRateHelper.GetExchRate("USD", ExchRateType.BanknoteSelling).ToString());
+            Console.WriteLine("");
+
+            // Other
+            Console.WriteLine("EUR - Forex Buying: " + exchRateHelper.GetExchRate("EUR", ExchRateType.ForexBuying).ToString());
+            Console.WriteLine("GBP - Forex Buying: " + exchRateHelper.GetExchRate("GBP", ExchRateType.ForexBuying).ToString());
+            Console.WriteLine("CAD - Forex Buying: " + exchRateHelper.GetExchRate("CAD", ExchRateType.ForexBuying).ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/TRYExchRate/TRYExchRate.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Xml;
+
+namespace TRYExchRate
+{
+    /// <summary>
+    /// T.C. Merkez Bankası tarafından yayınlanan döviz kurlarını okumak için kullanılan açık kaynak bir C# sınıfıdır.
+    /// </summary>
+    class TRYExchRate
+    {
+        private const string ApiBaseUrl = "http://www.tcmb.gov.tr/kurlar/{0}.xml";
+
+        public string ApiUrl { get; set; }
+        public DateTime CurrencyDate { get; set; }
+        private XmlDocument XmlDoc { get; set; }
+
+        public TRYExchRate(DateTime currencyDate)
+        {
+            CurrencyDate = currencyDate;
+        }
+
+        public Decimal GetExchRate(string currency, ExchRateType exchRateType)
+        {
+            LoadXmlDoc();
+
+            System.Globalization.CultureInfo culInfo = new System.Globalization.CultureInfo("en-US", true);
+            string nodeStr = String.Format("Tarih_Date/Currency[@CurrencyCode='{0}']/{1}", currency.ToUpper(), GetExchRateTypeNodeStr(exchRateType));
+
+            return Decimal.Parse(XmlDoc.SelectSingleNode(nodeStr).InnerXml, culInfo);
+        }
+
+
+        private void GenerateApiUrl()
+        {
+            ApiUrl = String.Format(TRYExchRate.ApiBaseUrl, this.CurrencyDate.ToString("yyyyMM") + "/" + this.CurrencyDate.ToString("ddMMyyyy"));
+        }
+
+        public void LoadXmlDoc()
+        {
+            if (XmlDoc == null)
+            {
+                XmlDoc = new XmlDocument();
+
+                try
+                {
+                    GenerateApiUrl();
+                    XmlDoc.Load(ApiUrl);
+                }
+                catch
+                {
+                    throw new Exception("Cannot connect TCMB api right now. Api url: " + ApiUrl);
+                }
+            }
+        }
+
+        private string GetExchRateTypeNodeStr(ExchRateType exchRateType)
+        {
+            string ret = "";
+
+            switch (exchRateType)
+            {
+                case ExchRateType.ForexBuying:
+                    ret = "ForexBuying";
+                    break;
+
+                case ExchRateType.ForexSelling:
+                    ret = "ForexSelling";
+                    break;
+
+                case ExchRateType.BanknoteBuying:
+                    ret = "BanknoteBuying";
+                    break;
+
+                case ExchRateType.BanknoteSelling:
+                    ret = "BanknoteSelling";
+                    break;
+            }
+
+            return ret;
+        }
+    }
+
+    public enum ExchRateType
+    {
+        ForexBuying,
+        ForexSelling,
+        BanknoteBuying,
+        BanknoteSelling
+    }
+}
